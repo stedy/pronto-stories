238ca3e45957c3b372fe8c072fdb1722fdc3322b
stedy <zstednic@fhcrc.org>
2015-11-08 14:09:44 -0800

    map on main page as well

--- python/make_geojson.py
+++ python/make_geojson.py
@@ -1,20 +1,12 @@
-import json
+import geojson
+import csv
 
-with open("../data/pairwise_routes.json", 'rb') as pr:
-    original = json.load(pr)
-    for x in original:
-        turns = x['turns']
-        fc = [[turn[1], turn[0]] for turn in turns]
-        route = x['route'].replace(":", "_")
-        print route
-        outfile = "route{}.geojson".format(route)
-        with open(outfile, 'w') as at:
-            at.write('{\n')
-            at.write('"type": "FeatureCollection",\n')
-            at.write('"crs": { "type": "name", "properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84" } },\n')
-            at.write('"features":[\n')
-            for f in fc[:-1]:
-                at.write('{{ "type": "Feature", "properties": {{  "id": "{id}", "name":"Along route"}}, "geometry": {{ "type": "Point", "coordinates":{latlong} }} }} ,\n'.format(id=route, latlong=f))
-            at.write('{{ "type": "Feature", "properties": {{  "id": "{id}", "name":"Along route"}}, "geometry": {{ "type": "Point", "coordinates": {latlong} }} }}\n'.format(id=route, latlong=fc[-1]))
-            at.write(']}')
+with open("../data/2015_station_data.csv") as sd:
+    reader = csv.reader(sd)
+    
+    for row in reader:
+        to_geojson['terminal'] = row[2]
+        to_geojson['lat'] = row[3]
+        to_geojson['long'] = row[4]
 
+print to_geojson
