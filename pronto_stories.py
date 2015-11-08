ff4e42810c0f57bbc44b87b1c8fd2b96fe97d841
stedy <zstednic@fhcrc.org>
2015-11-07 16:05:56 -0800

    starting the flask webapp

---
+++ pronto_stories.py
@@ -0,0 +1,65 @@
+import datetime as dt
+import os
+from sqlite3 import dbapi2 as sqlite3
+from flask import Flask, request, g, redirect, url_for,\
+        render_template, flash
+
+app = Flask(__name__)
+
+# Load default config and override config from an environment
+# variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'pronto.db'),
+    DEBUG=True,
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('ABOUNCE_SETTINGS', silent=True)
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql',mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+@app.route('/', methods=['GET', 'POST'])
+def show_entries():
+    db = get_db()
+    cur = db.execute("""SELECT * FROM Trips""")
+    entries = cur.fetchall()
+    return render_template('main.html', entries=entries)
+
+@app.route('/get_map', methods=['GET', 'POST'])
+def get_map():
+    db = get_db()
+    cur = db.execute("""SELECT station_start, station_end FROM Trips WHERE
+            start = ? AND end = ?""",
+            [request.form['start'], request.form['end']])
+    print(request.form['start'])
+    entries = cur.fetchall()
+    return render_template('map.html', entries=entries)
+    
+
+if __name__ == '__main__':
+    app.run()
