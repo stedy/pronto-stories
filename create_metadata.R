cfdc79cc574b539020dd28d42b00d90aab6ce7fc
stedy <zstednic@fhcrc.org>
2015-11-07 16:05:05 -0800

    better naming for sql tables

--- create_metadata.R
+++ create_metadata.R
@@ -3,12 +3,12 @@ library(dplyr)
 
 stations <- read.csv("data/2015_station_data.csv")
 trips <- read.csv("data/2015_trip_data.csv")
-trips$route.id <- paste(trips$from_station_id, trips$to_station_id, sep="_")
+trips$route <- paste(trips$from_station_id, trips$to_station_id, sep="_")
 
 trips.summary <-
   trips %>%
-  group_by(route.id) %>%
-  do(data.frame(ntrips = length(.$route.id),
+  group_by(route) %>%
+  do(data.frame(ntrips = length(.$route),
                 mean.time = mean(.$tripduration))) %>%
   mutate(routefraction = ntrips/nrow(trips))
 
@@ -16,27 +16,27 @@ routes <- gtools::permutations(54, 2, stations$terminal)
 all.routes <- c(paste(routes[, 1], routes[, 2], sep="_"),
                 paste(stations$id, stations$id, sep="_"))
 
-riderless.routes <- data.frame(route.id = setdiff(trips.summary$route.id, all.routes),
+riderless.routes <- data.frame(route = setdiff(trips.summary$route, all.routes),
                                ntrips=0, mean.time = 0, routefraction=NA)
 trips.summary <- rbind(trips.summary, riderless.routes)
 trips.summary$routerank <- rank(trips.summary$routefraction, ties.method="max")
 
-trips.summary$start <- sapply(strsplit(trips.summary$route.id, "_"), "[", 1)
-trips.summary$end <- sapply(strsplit(trips.summary$route.id, "_"), "[", 2)
+trips.summary$start <- sapply(strsplit(trips.summary$route, "_"), "[", 1)
+trips.summary$end <- sapply(strsplit(trips.summary$route, "_"), "[", 2)
 
 trips.summary <-
   trips.summary %>%
   merge(stations, by.x="start", by.y="terminal") %>%
-  rename(station.start=name) %>%
-  select(route.id, ntrips, mean.time, routefraction, routerank,
-         station.start, end)
+  rename(station_start=name) %>%
+  select(route, ntrips, mean.time, routefraction, routerank,
+         station_start, start, end)
 
 trips.summary <-
   trips.summary %>%
   merge(stations, by.x="end", by.y="terminal") %>%
-  rename(station.end=name) %>%
-  select(route.id, ntrips, mean.time, routefraction, routerank,
-         station.start, station.end)
+  rename(station_end=name) %>%
+  select(route, ntrips, mean.time, routefraction, routerank,
+         station_start, station_end, start, end)
 
 conn <- dbConnect(SQLite(), dbname = "pronto.db")
 dbSendQuery(conn, "DROP TABLE IF EXISTS Trips");
