4bb62b7888b783e5fc70b76f486bf6afa75ba2e2
stedy <zstednic@fhcrc.org>
2015-11-08 22:04:05 -0800

    adding mean time in min, sec

--- create_metadata.R
+++ create_metadata.R
@@ -9,7 +9,7 @@ trips.summary <-
   trips %>%
   group_by(route) %>%
   do(data.frame(ntrips = length(.$route),
-                mean.time = mean(.$tripduration))) %>%
+                mean_time = mean(.$tripduration))) %>%
   mutate(routefraction = ntrips/nrow(trips))
 
 routes <- gtools::permutations(54, 2, stations$terminal)
@@ -17,7 +17,7 @@ all.routes <- c(paste(routes[, 1], routes[, 2], sep="_"),
                 paste(stations$id, stations$id, sep="_"))
 
 riderless.routes <- data.frame(route = setdiff(trips.summary$route, all.routes),
-                               ntrips=0, mean.time = 0, routefraction=NA)
+                               ntrips=0, mean_time = 0, routefraction=NA)
 trips.summary <- rbind(trips.summary, riderless.routes)
 trips.summary$routerank <- rank(trips.summary$routefraction, ties.method="max")
 
@@ -28,15 +28,21 @@ trips.summary <-
   trips.summary %>%
   merge(stations, by.x="start", by.y="terminal") %>%
   rename(station_start=name) %>%
-  select(route, ntrips, mean.time, routefraction, routerank,
-         station_start, start, end)
+  select(route, ntrips, mean_time, routefraction, routerank,
+         station_start, start, end) %>%
+  mutate(minutes = floor(mean_time/60),
+         seconds = (mean_time %% 1) * 100)
 
 trips.summary <-
   trips.summary %>%
   merge(stations, by.x="end", by.y="terminal") %>%
   rename(station_end=name) %>%
-  select(route, ntrips, mean.time, routefraction, routerank,
-         station_start, station_end, start, end)
+  select(route, ntrips, mean_time, routefraction, routerank,
+         station_start, station_end, start, end,
+         minutes, seconds)
+trips.summary
+trips.summary[] <- data.frame(sapply(trips.summary, as.character))
+trips.summary$seconds <- as.numeric(trips.summary$seconds)
 
 conn <- dbConnect(SQLite(), dbname = "pronto.db")
 dbSendQuery(conn, "DROP TABLE IF EXISTS Trips");
