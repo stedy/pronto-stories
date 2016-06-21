661fd4873ec3d11bb867c5322a0f423aa6c16c7a
Patrick Finnigan <patrick.finnigan@hbo.com>
2016-06-20 23:10:57 -0700

    hacky marker click handling

--- static/StationPicker.jsx
+++ static/StationPicker.jsx
@@ -1,9 +1,19 @@
+var currentPicker;
 
 var StationPicker = React.createClass({
+  getInitialState: function () {
+    return {
+      value: ""
+    }
+  },
+  onClick: function () {
+    currentPicker = this;
+  },
   render: function () {
     return (
       <div id={this.props.id}>
-        <input className="typeahead" type="text" placeholder={this.props.placeholder} name={this.props.name}></input>
+        <input className="typeahead" type="text" placeholder={this.props.placeholder} name={this.props.name} value={this.state.value}></input>
+        <input type="button" onClick={this.onClick.bind(this)} value="Select"></input>
       </div>
     );
   }
@@ -12,10 +22,15 @@ var StationPicker = React.createClass({
 var StationForm = React.createClass({
   componentDidMount: function () {
     registerTypeaheads();
+    markerClickHandler = function (name) {
+      if (currentPicker !== undefined) {
+        currentPicker.setState({ value: name });
+      }
+    }
   },
   render: function() {
     return (
-      <form action="{{ url_for('get_map') }}" method="post">
+      <form action={this.props.action} method="post">
         <p>Select a start and stop station from the dropdown menu to view trip statistics for that route.</p>
         <StationPicker id="start-stations" placeholder="Starting Station" name="start"></StationPicker>
         <StationPicker id="end-stations" placeholder="Ending Station" name="end"></StationPicker>
@@ -25,7 +40,8 @@ var StationForm = React.createClass({
   }
 });
 
+var actionUrl = $('#hidden_get_map_link').attr("href");
 ReactDOM.render(
-  <StationForm />,
+  <StationForm action={actionUrl} />,
   document.getElementById('stationPickerPlaceholder')
 );
