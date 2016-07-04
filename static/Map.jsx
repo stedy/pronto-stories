c63cb2f2eef925e1a9bcf4f64ed0ab50c59fb3fc
Patrick Finnigan <patrick.finnigan@hbo.com>
2016-07-03 18:46:15 -0700

    fix whitespace

--- static/Map.jsx
+++ static/Map.jsx
@@ -15,21 +15,21 @@ window.Map = React.createClass({
     var geojsonLayer = L.geoJson(geojson, {
         style: function(feature) {
           return {color: feature.properties.visit};
-          },
+        },
         pointToLayer: function(feature, latlng) {
-      var currentZoom = that.map.getZoom();
+          var currentZoom = that.map.getZoom();
 
-    var marker = new L.CircleMarker(latlng, {radius: 10, fillOpacity: 0.85});
-    marker.on('click', function () {
-      if (window.markerClickHandler !== undefined) {
-        window.markerClickHandler(feature.properties.name);
-      }
-    });
-    return marker;
-    },
-    onEachFeature: function (feature, layer) {
-    layer.bindPopup(feature.properties.name);
-    }
+          var marker = new L.CircleMarker(latlng, {radius: 10, fillOpacity: 0.85});
+          marker.on('click', function () {
+            if (window.markerClickHandler !== undefined) {
+              window.markerClickHandler(feature.properties.name);
+            }
+          });
+          return marker;
+        },
+        onEachFeature: function (feature, layer) {
+          layer.bindPopup(feature.properties.name);
+        }
     });
     that.map.addLayer(geojsonLayer);
 
@@ -43,120 +43,121 @@ window.Map = React.createClass({
 
     var svg = d3.select(that.map.getPanes().overlayPane).append('svg');
     var g = svg.append('g').attr('class', 'leaflet-zoom-hide');
-d3.json(this.props.routeUrl, function(collection) {
 
-    var featuresdata = collection.features.filter(function(d) {
-      return d.properties.id == that.props.route
-        })
+    d3.json(this.props.routeUrl, function(collection) {
 
-        var transform = d3.geo.transform({
-            point: projectPoint
-        });
+      var featuresdata = collection.features.filter(function(d) {
+        return d.properties.id == that.props.route;
+      });
 
-        var d3path = d3.geo.path().projection(transform);
+      var transform = d3.geo.transform({
+          point: projectPoint
+      });
 
-        var toLine = d3.svg.line()
-            .interpolate('linear')
-            .x(function(d) {
-                return applyLatLngToLayer(d).x
-            })
-            .y(function(d) {
-                return applyLatLngToLayer(d).y
-            });
+      var d3path = d3.geo.path().projection(transform);
 
-        var ptFeatures = g.selectAll('circle')
-            .data(featuresdata)
-            .enter()
-            .append('circle')
-            .attr('class', 'waypoints');
+      var toLine = d3.svg.line()
+          .interpolate('linear')
+          .x(function(d) {
+              return applyLatLngToLayer(d).x
+          })
+          .y(function(d) {
+              return applyLatLngToLayer(d).y
+          });
 
-        var linePath = g.selectAll('.lineConnect')
-            .data([featuresdata])
-            .enter()
-            .append('path')
-            .attr('class', 'lineConnect');
+      var ptFeatures = g.selectAll('circle')
+          .data(featuresdata)
+          .enter()
+          .append('circle')
+          .attr('class', 'waypoints');
 
-        var marker = g.append('circle')
-            .attr('id', 'marker')
-            .attr('class', 'travelMarker');
+      var linePath = g.selectAll('.lineConnect')
+          .data([featuresdata])
+          .enter()
+          .append('path')
+          .attr('class', 'lineConnect');
 
-        var originANDdestination = [featuresdata[0], featuresdata[featuresdata.length - 1]]
+      var marker = g.append('circle')
+          .attr('id', 'marker')
+          .attr('class', 'travelMarker');
 
-        var begend = g.selectAll('.drinks')
-            .data(originANDdestination)
-            .enter()
-            .append('circle', '.drinks')
-            .style('opacity', '1');
+      var originANDdestination = [featuresdata[0], featuresdata[featuresdata.length - 1]]
 
-        that.map.on('viewreset', reset);
+      var begend = g.selectAll('.drinks')
+          .data(originANDdestination)
+          .enter()
+          .append('circle', '.drinks')
+          .style('opacity', '1');
 
-        reset();
-        transition();
+      that.map.on('viewreset', reset);
 
-        function reset() {
-            var bounds = d3path.bounds(collection),
-                topLeft = bounds[0],
-                bottomRight = bounds[1];
+      reset();
+      transition();
 
-            begend.attr('transform',
-                function(d) {
-                    return 'translate(' +
-                        applyLatLngToLayer(d).x + ',' +
-                        applyLatLngToLayer(d).y + ')';
-                });
+      function reset() {
+          var bounds = d3path.bounds(collection),
+              topLeft = bounds[0],
+              bottomRight = bounds[1];
 
-            ptFeatures.attr('transform',
-                function(d) {
-                    return 'translate(' +
-                        applyLatLngToLayer(d).x + ',' +
-                        applyLatLngToLayer(d).y + ')';
-                });
+          begend.attr('transform',
+              function(d) {
+                  return 'translate(' +
+                      applyLatLngToLayer(d).x + ',' +
+                      applyLatLngToLayer(d).y + ')';
+              });
 
-            marker.attr('transform',
-                function() {
-                    var y = featuresdata[0].geometry.coordinates[0]
-                    var x = featuresdata[0].geometry.coordinates[1]
-                    return 'translate(' +
-                        that.map.latLngToLayerPoint(new L.LatLng(y, x)).x + ',' +
-                        that.map.latLngToLayerPoint(new L.LatLng(y, x)).y + ')';
-                });
+          ptFeatures.attr('transform',
+              function(d) {
+                  return 'translate(' +
+                      applyLatLngToLayer(d).x + ',' +
+                      applyLatLngToLayer(d).y + ')';
+              });
+
+          marker.attr('transform',
+              function() {
+                  var y = featuresdata[0].geometry.coordinates[0]
+                  var x = featuresdata[0].geometry.coordinates[1]
+                  return 'translate(' +
+                      that.map.latLngToLayerPoint(new L.LatLng(y, x)).x + ',' +
+                      that.map.latLngToLayerPoint(new L.LatLng(y, x)).y + ')';
+              });
 
 
-            svg.attr('width', bottomRight[0] - topLeft[0] + 120)
-                .attr('height', bottomRight[1] - topLeft[1] + 120)
-                .style('left', topLeft[0] - 50 + 'px')
-                .style('top', topLeft[1] - 50 + 'px');
+          svg.attr('width', bottomRight[0] - topLeft[0] + 120)
+              .attr('height', bottomRight[1] - topLeft[1] + 120)
+              .style('left', topLeft[0] - 50 + 'px')
+              .style('top', topLeft[1] - 50 + 'px');
 
-            linePath.attr('d', toLine)
-            g.attr('transform', 'translate(' + (-topLeft[0] + 50) + ',' + (-topLeft[1] + 50) + ')');
+          linePath.attr('d', toLine)
+          g.attr('transform', 'translate(' + (-topLeft[0] + 50) + ',' + (-topLeft[1] + 50) + ')');
 
-        } // end reset
+      } // end reset
 
-        function transition() {
-            linePath.transition()
-                .duration(7500)
-                .attrTween('stroke-dasharray', tweenDash)
-        }
+      function transition() {
+          linePath.transition()
+              .duration(7500)
+              .attrTween('stroke-dasharray', tweenDash)
+      }
 
-        function tweenDash() {
-            return function(t) {
-                //total length of path (single value)
-                var l = linePath.node().getTotalLength();
-                var interpolate = d3.interpolateString('0,' + l, l + ',' + l);
-                var marker = d3.select('#marker');
-                var p = linePath.node().getPointAtLength(t * l);
+      function tweenDash() {
+          return function(t) {
+              //total length of path (single value)
+              var l = linePath.node().getTotalLength();
+              var interpolate = d3.interpolateString('0,' + l, l + ',' + l);
+              var marker = d3.select('#marker');
+              var p = linePath.node().getPointAtLength(t * l);
 
-                //Move the marker to that point
-                marker.attr('transform', 'translate(' + p.x + ',' + p.y + ')'); //move marker
-                console.log(interpolate(t))
-                return interpolate(t);
-            }
-        }
+              //Move the marker to that point
+              marker.attr('transform', 'translate(' + p.x + ',' + p.y + ')'); //move marker
+              console.log(interpolate(t))
+              return interpolate(t);
+          }
+      }
 
-        function projectPoint(x, y) {
-            var point = that.map.latLngToLayerPoint(new L.LatLng(x,y));
-            this.stream.point(point.x, point.y);
-        } //end projectPoint
+      function projectPoint(x, y) {
+          var point = that.map.latLngToLayerPoint(new L.LatLng(x,y));
+          this.stream.point(point.x, point.y);
+      } //end projectPoint
     });
 
     function applyLatLngToLayer(d) {
