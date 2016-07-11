c37e6e2bdafcc918fd98b5fa3c49bf88e3804c30
Patrick Finnigan <patrick.finnigan@hbo.com>
2016-07-11 06:42:52 -0700

    cleanup Elevations.jsx

--- static/Elevations.jsx
+++ static/Elevations.jsx
@@ -6,29 +6,11 @@ window.Elevations = React.createClass({
       .attr("width", 300)
       .attr("height", 500);
 
+    // Temporary placeholder. Once all geojson elevations data is available,
+    // parameterize this.
     d3.json('elevationsBT-01_CBD-07.geojson', function (elevations) {
       var lineData = [];
-      // var scaleMax = 500;
-      // var scaleMin = 0;
-      // var actualMax = Number.MIN_VALUE;
-      // var actualMin = Number.MAX_VALUE;
-      // for (var i = 0; i < elevations.length; ++i) {
-      //   var elevation = elevations[i];
-      //   if (elevation < actualMin) {
-      //     actualMin = elevation;
-      //   }
-      //   if (elevation > actualMax) {
-      //     actualMax = elevation;
-      //   }
-      // }
-
-      // var delta = actualMax - actualMin;
-      // var scaleFactor = (scaleMax - scaleMin) / delta;
-
-      // var yMin = Number.MAX_VALUE;
-      // var yMax = Number.MIN_VALUE;
       for (var i = 0; i < elevations.length; ++i) {
-        //var y = ((elevations[i] - actualMin) * scaleFactor);
         lineData.push({ x: i*3, y: elevations[i] });
       }
 
@@ -42,40 +24,6 @@ window.Elevations = React.createClass({
         .attr("stroke", "blue")
         .attr("stroke-width", 2)
         .attr("fill", "none");
-
-      //alert("actualMin: " + actualMin + ", actualMax: " + actualMax + ", scaleFactor: " + scaleFactor + ", yMin: " + yMin + ", yMax: " + yMax);
-
-      // plotElevation(elevations);
-
-      // // Takes an array of ElevationResult objects, draws the path on the map
-      // // and plots the elevation profile on a Visualization API ColumnChart.
-      // function plotElevation(elevations) {
-      //   var chartDiv = document.getElementById('otherPlaceholder');
-
-      //   // Create a new chart in the elevation_chart DIV.
-      //   var chart = new google.visualization.ColumnChart(chartDiv);
-
-      //   // Extract the data from which to populate the chart.
-      //   // Because the samples are equidistant, the 'Sample'
-      //   // column here does double duty as distance along the
-      //   // X axis.
-      //   var data = new google.visualization.DataTable();
-      //   data.addColumn('string', 'Sample');
-      //   data.addColumn('number', 'Elevation');
-      //   for (var i = 0; i < elevations.length; i++) {
-      //     data.addRow(['', elevations[i]]);
-
-      //   }
-
-      //   // Draw the chart using the data within its DIV.
-      //   chart.draw(data, {
-      //     height: 150,
-      //     width: 400,
-      //     legend: 'none',
-      //     titleY: 'Elevation (m)'
-      //   });
-      // }
-
     });
   },
   render: function() {
@@ -84,4 +32,3 @@ window.Elevations = React.createClass({
     );
   }
 });
-
